edeccdda4dc2e09482e5a3c9396f8f85a4129ea2
Sagar M Bandennavar <53654068+SagarMB09@users.noreply.github.com>
2020-12-09 15:56:48 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+function loadUrls(){
+
+}
+
+function saveUrls(){
+
+}
+document.getElementById('button').addEventListener('click', loadUrls);
+document.getElementById('button').addEventListener('click', saveUrls);
